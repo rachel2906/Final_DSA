ea18d6f1bb4a027575472cb142e24283c49b7fd1
rachel <24110091@student.hcmute.edu.vn>
2026-01-08 17:38:33 +0700

    Update HeapSort.cpp

--- HeapSort.cpp
+++ HeapSort.cpp
@@ -1,28 +1,12 @@
-#include "SortAlgorithm.h"
+#include "HeapSort.h"
 #include <algorithm>
 
-class HeapSort : public SortAlgorithm {
-public:
-    void sort(std::vector<int>& a, CostModel& cost) override {
-        std::make_heap(a.begin(), a.end(), [&](int x, int y) {
-            cost.cpu_compare++;
-            return x > y;
-        });
-        cost.heap_op += a.size();
+void HeapSort::sort(std::vector<int>& data) {
+    // Build max heap
+    std::make_heap(data.begin(), data.end());
 
-        for (int i = (int)a.size(); i > 1; --i) {
-            std::pop_heap(a.begin(), a.begin() + i);
-            cost.heap_op++;
-            std::swap(a[0], a[i - 1]);
-            cost.cpu_move++;
-        }
+    // Pop elements one by one to sort
+    for (auto it = data.end(); it != data.begin(); --it) {
+        std::pop_heap(data.begin(), it);
     }
-
-    const char* name() const override {
-        return "HeapSort (Chunk)";
-    }
-};
-
-SortAlgorithm* createHeapSort() {
-    return new HeapSort();
 }
