e95743e1623f0cbbdc6aee627ed654f7b4c46c33
rachel <24110091@student.hcmute.edu.vn>
2026-01-09 13:23:49 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,18 +1,46 @@
 #include "Analyzer.h"
 #include "ExternalMergeSort.h"
 #include <iostream>
+#include <fstream>
+#include <random>
+#include <string>
+
+void createData(const std::string& path, int n, int type) {
+    std::ofstream out(path);
+    if (!out) return;
+
+    if (type == 1) { 
+        std::random_device rd; std::mt19937 gen(rd());
+        std::uniform_int_distribution<> dis(1, 1000000);
+        for (int i = 0; i < n; ++i) out << dis(gen) << "\n";
+    } 
+    else if (type == 2) { 
+        for (int i = 0; i < n; ++i) out << i << "\n";
+    } 
+    else if (type == 3) { 
+        for (int i = n; i > 0; --i) out << i << "\n";
+    }
+    out.close();
+}
 
 int main() {
     Analyzer::explain();
+    int size, chunk, type;
 
-    ExternalMergeSort sorter(100000);
-    
-    std::string input = "../data/input.txt";
-    std::string output = "../data/output.txt";
+    std::cout << "\n[DEMO MODE]\n";
+    std::cout << "1. Random | 2. Sorted | 3. Reverse: ";
+    std::cin >> type;
+    std::cout << "Nhap so luong phan tu: ";
+    std::cin >> size;
+    std::cout << "Nhap chunkSize: ";
+    std::cin >> chunk;
 
-    std::cout << "Processing Big Data File...\n";
-    sorter.sortFile(input, output);
+    createData("../data/input.txt", size, type); 
+
+    ExternalMergeSort sorter(chunk);
+    std::cout << "\nProcessing...\n";
+    sorter.sortFile("../data/input.txt", "../data/output.txt");
+
     std::cout << "Sorting completed successfully.\n";
-
     return 0;
 }
