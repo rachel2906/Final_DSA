0266e322b3ec8c37f47bdecbff59f994fee25ac8
rachel <24110091@student.hcmute.edu.vn>
2026-01-09 12:04:47 +0700

    Refactor SortAlgorithm.h for formatting consistency

--- SortAlgorithm.h
+++ SortAlgorithm.h
@@ -5,10 +5,6 @@
 class SortAlgorithm {
 public:
     virtual ~SortAlgorithm() = default;
-
-    // Sort data in memory
     virtual void sort(std::vector<int>& data) = 0;
-
-    // Name of algorithm (for analysis & explanation)
     virtual std::string name() const = 0;
 };
