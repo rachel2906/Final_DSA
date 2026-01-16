25ea5ec2488dfb1156709bc893ea2583f2be76b3
24110139 <24110139@student.hcmute.edu.vn>
2026-01-16 13:04:41 +0700

    Update HeapSort.cpp

--- HeapSort.cpp
+++ HeapSort.cpp
@@ -1,10 +1,28 @@
 #include "HeapSort.h"
-#include <algorithm>
+#include <algorithm> // std::swap
 
-void HeapSort::sort(std::vector<int>& data) {
-    // Phase 1: Biến vector thành Max-Heap (O(n))
-    std::make_heap(data.begin(), data.end());
+void HeapSort::heapify(std::vector<int>& arr, int n, int i) {
+    int largest = i;        // giả sử node hiện tại là lớn nhất
+    int left = 2 * i + 1;   // con trái
+    int right = 2 * i + 2;  // con phải
 
-    // Phase 2: Sắp xếp Heap thành mảng tăng dần (O(n log n))
-    std::sort_heap(data.begin(), data.end());
+    // nếu con trái lớn hơn cha
+    if (left < n && arr[left] > arr[largest]) {
+        largest = left;
+    }
+
+    // nếu con phải lớn hơn cha hiện tại
+    if (right < n && arr[right] > arr[largest]) {
+        largest = right;
+    }
+
+    // nếu cha không còn là lớn nhất
+    if (largest != i) {
+        std::swap(arr[i], arr[largest]);
+
+        // tiếp tục sửa cây con bị ảnh hưởng
+        heapify(arr, n, largest);
+    }
 }
+
+
